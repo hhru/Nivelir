604abe23a4400fe81048d5f203961b683a421704
Almaz Ibragimov <almazrafi@gmail.com>
2022-06-16 09:14:23 +0300

    Sharing for ScreenShowSafariAction

--- Sources/Addons/Safari/Safari.swift
+++ Sources/Addons/Safari/Safari.swift
@@ -4,8 +4,8 @@ import SafariServices
 
 public struct Safari: CustomStringConvertible {
 
-    private let anyConfiguration: Any?
-    private let anyDismissButtonStyle: Any?
+    private let storedConfiguration: Any?
+    private let storedDismissButtonStyle: Any?
 
     public let url: URL
 
@@ -18,14 +18,21 @@ public struct Safari: CustomStringConvertible {
     public let didFinish: (() -> Void)?
     public let willOpenInBrowser: (() -> Void)?
 
+    public let sharingApplicationActivities: ((_ url: URL, _ title: String?) -> [SharingActivity])?
+    public let sharingExcludedActivityTypes: ((_ url: URL, _ title: String?) -> [SharingActivityType])?
+
     @available(iOS 11.0, *)
-    public var configuration: SFSafariViewController.Configuration? {
-        anyConfiguration as? SFSafariViewController.Configuration
+    public var configuration: SFSafariViewController.Configuration {
+        storedConfiguration.flatMap { configuration in
+            configuration as? SFSafariViewController.Configuration
+        } ?? SFSafariViewController.Configuration()
     }
 
     @available(iOS 11.0, *)
-    public var dismissButtonStyle: SFSafariViewController.DismissButtonStyle? {
-        anyDismissButtonStyle as? SFSafariViewController.DismissButtonStyle
+    public var dismissButtonStyle: SFSafariViewController.DismissButtonStyle {
+        storedDismissButtonStyle.flatMap { dismissButtonStyle in
+            dismissButtonStyle as? SFSafariViewController.DismissButtonStyle
+        } ?? .done
     }
 
     public var description: String {
@@ -40,12 +47,14 @@ public struct Safari: CustomStringConvertible {
         didCompleteInitialLoad: ((_ successfully: Bool) -> Void)? = nil,
         didRedirectInitialLoad: ((_ url: URL) -> Void)? = nil,
         didFinish: (() -> Void)? = nil,
-        willOpenInBrowser: (() -> Void)? = nil
+        willOpenInBrowser: (() -> Void)? = nil,
+        sharingApplicationActivities: ((_ url: URL, _ title: String?) -> [SharingActivity])? = nil,
+        sharingExcludedActivityTypes: ((_ url: URL, _ title: String?) -> [SharingActivityType])? = nil
     ) {
         self.url = url
 
-        self.anyConfiguration = nil
-        self.anyDismissButtonStyle = nil
+        self.storedConfiguration = nil
+        self.storedDismissButtonStyle = nil
 
         self.preferredBarTintColor = preferredBarTintColor
         self.preferredControlTintColor = preferredControlTintColor
@@ -55,25 +64,30 @@ public struct Safari: CustomStringConvertible {
         self.didRedirectInitialLoad = didRedirectInitialLoad
         self.didFinish = didFinish
         self.willOpenInBrowser = willOpenInBrowser
+
+        self.sharingApplicationActivities = sharingApplicationActivities
+        self.sharingExcludedActivityTypes = sharingExcludedActivityTypes
     }
 
     @available(iOS 11.0, *)
     public init(
         url: URL,
         configuration: SFSafariViewController.Configuration,
-        dismissButtonStyle: SFSafariViewController.DismissButtonStyle? = nil,
+        dismissButtonStyle: SFSafariViewController.DismissButtonStyle = .done,
         preferredBarTintColor: UIColor? = nil,
         preferredControlTintColor: UIColor? = nil,
         didInitialize: ((_ container: SFSafariViewController) -> Void)? = nil,
         didCompleteInitialLoad: ((_ successfully: Bool) -> Void)? = nil,
         didRedirectInitialLoad: ((_ url: URL) -> Void)? = nil,
         didFinish: (() -> Void)? = nil,
-        willOpenInBrowser: (() -> Void)? = nil
+        willOpenInBrowser: (() -> Void)? = nil,
+        sharingApplicationActivities: ((_ url: URL, _ title: String?) -> [SharingActivity])? = nil,
+        sharingExcludedActivityTypes: ((_ url: URL, _ title: String?) -> [SharingActivityType])? = nil
     ) {
         self.url = url
 
-        self.anyConfiguration = configuration
-        self.anyDismissButtonStyle = dismissButtonStyle
+        self.storedConfiguration = configuration
+        self.storedDismissButtonStyle = dismissButtonStyle
 
         self.preferredBarTintColor = preferredBarTintColor
         self.preferredControlTintColor = preferredControlTintColor
@@ -83,6 +97,9 @@ public struct Safari: CustomStringConvertible {
         self.didRedirectInitialLoad = didRedirectInitialLoad
         self.didFinish = didFinish
         self.willOpenInBrowser = willOpenInBrowser
+
+        self.sharingApplicationActivities = sharingApplicationActivities
+        self.sharingExcludedActivityTypes = sharingExcludedActivityTypes
     }
 }
 #endif
