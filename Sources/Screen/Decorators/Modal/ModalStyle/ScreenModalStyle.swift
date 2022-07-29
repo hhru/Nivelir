cf5e1472bd50b73558cea5af963e1d14c910d9a8
Timur Shafigullin <hometim55@gmail.com>
2022-07-29 14:29:49 +0300

    Added documentation for ScreenModalStyleDecorator

--- Sources/Screen/Decorators/Modal/ModalStyle/ScreenModalStyle.swift
+++ Sources/Screen/Decorators/Modal/ModalStyle/ScreenModalStyle.swift
@@ -1,13 +1,17 @@
 #if canImport(UIKit)
 import UIKit
 
+/// Modal screen presentation style.
+/// Changes the presentation animation when the screen is shown modally.
 public enum ScreenModalStyle {
 
+    /// Default animation using `UIModalPresentationStyle` and `UIModalTransitionStyle`.
     case `default`(
         presentation: UIModalPresentationStyle? = nil,
         transition: UIModalTransitionStyle? = nil
     )
 
+    /// Custom animation using `UIViewControllerTransitioningDelegate` implementation.
     case custom(delegate: UIViewControllerTransitioningDelegate)
 }
 #endif
--- Sources/Screen/Decorators/Modal/ModalStyle/ScreenModalStyleDecorator.swift
+++ Sources/Screen/Decorators/Modal/ModalStyle/ScreenModalStyleDecorator.swift
@@ -1,6 +1,9 @@
 #if canImport(UIKit)
 import UIKit
 
+/// Configuring the modal screen presentation for a container with type `UIViewController`.
+///
+/// - SeeAlso: `ScreenModalStyle`
 public struct ScreenModalStyleDecorator<Container: UIViewController>: ScreenDecorator {
 
     public let style: ScreenModalStyle
@@ -50,18 +53,54 @@ public struct ScreenModalStyleDecorator<Container: UIViewController>: ScreenDeco
 
 extension Screen where Container: UIViewController {
 
+    /// Configuring the modal screen presentation.
+    ///
+    /// - Parameter style: Type of modal presentation style.
+    /// - Returns: New `Screen`, which will be animated with the selected style when presented.
     public func withModalStyle(_ style: ScreenModalStyle) -> AnyScreen<Container> {
         decorated(by: ScreenModalStyleDecorator(style: style))
     }
 
+    /// Sets the `modalPresentationStyle` of `UIViewController` property for the container.
+    /// - Parameter style: Modal presentation styles available when presenting view controllers.
+    /// - Returns: New `Screen` with the `modalPresentationStyle` property set.
+    ///
+    /// - SeeAlso: `UIModalPresentationStyle`
     public func withModalPresentationStyle(_ style: UIModalPresentationStyle) -> AnyScreen<Container> {
         withModalStyle(.default(presentation: style))
     }
 
+    /// Sets the `modalTransitionStyle` of `UIViewController` property for the container.
+    /// - Parameter style: Transition styles available when presenting view controllers.
+    /// - Returns: New `Screen` with the `modalTransitionStyle` property set.
+    ///
+    /// - SeeAlso: `UIModalTransitionStyle`
     public func withModalTransitionStyle(_ style: UIModalTransitionStyle) -> AnyScreen<Container> {
         withModalStyle(.default(transition: style))
     }
 
+    /// Sets the `transitioningDelegate` of `UIViewController` property for the container.
+    /// Also, the `modalPresentationStyle` property will be set to `.custom`.
+    ///
+    /// Usage example, where `CardModalTransitionAnimator` is an
+    /// implementation of the `UIViewControllerTransitioningDelegate` protocol:
+    ///
+    /// ```swift
+    /// navigator.navigate(from: container) { route in
+    ///    route.present(
+    ///        ChatScreen(chatID: 1)
+    ///            .withModalTransitioningDelegate(CardModalTransitionAnimator())
+    ///    )
+    /// }
+    /// ```
+    ///
+    /// - Parameter delegate: `UIViewControllerTransitioningDelegate` protocol
+    /// implementation for custom modal screen presentation.
+    /// The implementation class can be safely initialized in the parameter by the place of use,
+    /// even though the `transitioningDelegate` property is weak.
+    /// The lifecycle of the implementation class will correspond to the lifecycle of the screen container.
+    ///
+    /// - Returns: New `Screen` with the `transitioningDelegate` property set.
     public func withModalTransitioningDelegate(
         _ delegate: UIViewControllerTransitioningDelegate
     ) -> AnyScreen<Container> {
