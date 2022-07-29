9bff5755f344b9e46b7b4cf54230326c65d2aecc
Timur Shafigullin <hometim55@gmail.com>
2022-07-29 18:02:16 +0300

    Add documentation to ScreenTabsDecorator

--- Sources/Screen/Decorators/Tabs/ScreenTabsDecorator.swift
+++ Sources/Screen/Decorators/Tabs/ScreenTabsDecorator.swift
@@ -1,6 +1,8 @@
 #if canImport(UIKit)
 import UIKit
 
+/// Sets the root view controllers of the tab bar controller.
+/// Sets the `viewControllers` property for a container with type `UITabBarController`.
 public struct ScreenTabsDecorator<Container: UITabBarController>: ScreenDecorator {
 
     public let tabs: [AnyModalScreen]
@@ -33,21 +35,46 @@ public struct ScreenTabsDecorator<Container: UITabBarController>: ScreenDecorato
 
 extension Screen where Container: UITabBarController {
 
+    /// Sets the root view controllers of the tab bar controller.
+    /// Sets the `viewControllers` property for a container with type `UITabBarController`.
+    /// - Parameter tabs: The array of screens to display in the tab bar interface.
+    /// The order of the screens in this array corresponds to the display order in the tab bar,
+    /// with the screen at index 0 representing the left-most tab,
+    /// the screen at index 1 the next tab to the right, and so on.
+    /// - Returns: New `Screen` with updated `viewControllers` in container.
     public func withTabs(_ tabs: [AnyModalScreen]) -> AnyScreen<Container> {
         decorated(by: ScreenTabsDecorator(tabs: tabs))
     }
 
+    /// Sets the root view controllers of the tab bar controller.
+    /// Sets the `viewControllers` property for a container with type `UITabBarController`.
+    /// - Parameter tabs: The array of screens to display in the tab bar interface.
+    /// The order of the screens in this array corresponds to the display order in the tab bar,
+    /// with the screen at index 0 representing the left-most tab,
+    /// the screen at index 1 the next tab to the right, and so on.
+    /// - Returns: New `Screen` with updated `viewControllers` in container.
     public func withTabs(_ tabs: AnyModalScreen...) -> AnyScreen<Container> {
         withTabs(tabs)
     }
 
+    /// Sets the root view controller of the tab bar controller.
+    /// Sets the `viewControllers` property for a container with type `UITabBarController`.
+    /// - Parameter screen: The screen to display in the tab bar interface.
+    /// - Returns: New `Screen` with updated `viewControllers` in container.
     public func withTabs<T: Screen>(
         _ screen: T
     ) -> AnyScreen<Container> where T.Container: UIViewController {
         withTabs(screen.eraseToAnyModalScreen())
     }
 
-    // swiftlint:disable:next generic_type_name
+    // swiftlint:disable generic_type_name
+
+    /// Sets the root view controller of the tab bar controller.
+    /// Sets the `viewControllers` property for a container with type `UITabBarController`.
+    /// - Parameters:
+    ///   - screen0: The screen representing the left-most tab.
+    ///   - screen1: The screen of the next tab after `screen0`.
+    /// - Returns: New `Screen` with updated `viewControllers` in container.
     public func withTabs<T0: Screen, T1: Screen>(
         _ screen0: T0,
         _ screen1: T1
@@ -60,7 +87,13 @@ extension Screen where Container: UITabBarController {
         )
     }
 
-    // swiftlint:disable:next generic_type_name
+    /// Sets the root view controller of the tab bar controller.
+    /// Sets the `viewControllers` property for a container with type `UITabBarController`.
+    /// - Parameters:
+    ///   - screen0: The screen representing the left-most tab.
+    ///   - screen1: The screen of the next tab after `screen0`.
+    ///   - screen2: The screen of the next tab after `screen1`.
+    /// - Returns: New `Screen` with updated `viewControllers` in container.
     public func withTabs<T0: Screen, T1: Screen, T2: Screen>(
         _ screen0: T0,
         _ screen1: T1,
@@ -76,7 +109,14 @@ extension Screen where Container: UITabBarController {
         )
     }
 
-    // swiftlint:disable:next generic_type_name
+    /// Sets the root view controller of the tab bar controller.
+    /// Sets the `viewControllers` property for a container with type `UITabBarController`.
+    /// - Parameters:
+    ///   - screen0: The screen representing the left-most tab.
+    ///   - screen1: The screen of the next tab after `screen0`.
+    ///   - screen2: The screen of the next tab after `screen1`.
+    ///   - screen3: The screen of the next tab after `screen2`.
+    /// - Returns: New `Screen` with updated `viewControllers` in container.
     public func withTabs<T0: Screen, T1: Screen, T2: Screen, T3: Screen>(
         _ screen0: T0,
         _ screen1: T1,
@@ -95,7 +135,15 @@ extension Screen where Container: UITabBarController {
         )
     }
 
-    // swiftlint:disable:next generic_type_name
+    /// Sets the root view controller of the tab bar controller.
+    /// Sets the `viewControllers` property for a container with type `UITabBarController`.
+    /// - Parameters:
+    ///   - screen0: The screen representing the left-most tab.
+    ///   - screen1: The screen of the next tab after `screen0`.
+    ///   - screen2: The screen of the next tab after `screen1`.
+    ///   - screen3: The screen of the next tab after `screen2`.
+    ///   - screen4: The screen of the next tab after `screen3`.
+    /// - Returns: New `Screen` with updated `viewControllers` in container.
     public func withTabs<T0: Screen, T1: Screen, T2: Screen, T3: Screen, T4: Screen>(
         _ screen0: T0,
         _ screen1: T1,
@@ -116,5 +164,7 @@ extension Screen where Container: UITabBarController {
             screen4.eraseToAnyModalScreen()
         )
     }
+
+    // swiftlint:enable generic_type_name
 }
 #endif
