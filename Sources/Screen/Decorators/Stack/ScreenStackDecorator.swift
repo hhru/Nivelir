4d032cd6acf1eb25c8a3f92eaa4d1b642734530c
Timur Shafigullin <hometim55@gmail.com>
2022-07-29 17:19:38 +0300

    Add documentation to ScreenStackDecorator

--- Sources/Screen/Decorators/Stack/ScreenStackDecorator.swift
+++ Sources/Screen/Decorators/Stack/ScreenStackDecorator.swift
@@ -1,6 +1,8 @@
 #if canImport(UIKit)
 import UIKit
 
+/// Replaces the view controllers currently managed by the navigation controller with the specified screens.
+/// Sets the `viewControllers` property for a container with type `UINavigationController`.
 public struct ScreenStackDecorator<Container: UINavigationController>: ScreenDecorator {
 
     public let stack: [AnyModalScreen]
@@ -33,21 +35,46 @@ public struct ScreenStackDecorator<Container: UINavigationController>: ScreenDec
 
 extension Screen where Container: UINavigationController {
 
+    /// Replaces the view controllers currently managed by the navigation controller with the specified screens.
+    /// Sets the `viewControllers` property for a container with type `UINavigationController`.
+    /// - Parameter stack: The screens to place in the stack.
+    /// The front-to-back order of the screens in this array represents
+    /// the new bottom-to-top order of the controllers in the navigation stack.
+    /// Thus, the last item added to the array becomes the top item of the navigation stack.
+    /// - Returns: New `Screen` with replaced `viewControllers` in container.
     public func withStack(_ stack: [AnyModalScreen]) -> AnyScreen<Container> {
         decorated(by: ScreenStackDecorator(stack: stack))
     }
 
+    /// Replaces the view controllers currently managed by the navigation controller with the specified screens.
+    /// Sets the `viewControllers` property for a container with type `UINavigationController`.
+    /// - Parameter stack: The screens to place in the stack.
+    /// The front-to-back order of the screens in this array represents
+    /// the new bottom-to-top order of the controllers in the navigation stack.
+    /// Thus, the last item added to the array becomes the top item of the navigation stack.
+    /// - Returns: New `Screen` with replaced `viewControllers` in container.
     public func withStack(_ stack: AnyModalScreen...) -> AnyScreen<Container> {
         withStack(stack)
     }
 
+    /// Replaces the view controllers currently managed by the navigation controller with the specified screen.
+    /// Sets the `viewControllers` property for a container with type `UINavigationController`.
+    /// - Parameter screen: The screen to place in the stack as root.
+    /// - Returns: New `Screen` with replaced `viewControllers` in container.
     public func withStack<T: Screen>(
         _ screen: T
     ) -> AnyScreen<Container> where T.Container: UIViewController {
         withStack(screen.eraseToAnyModalScreen())
     }
 
-    // swiftlint:disable:next generic_type_name
+    // swiftlint:disable generic_type_name
+
+    /// Replaces the view controllers currently managed by the navigation controller with the specified screen.
+    /// Sets the `viewControllers` property for a container with type `UINavigationController`.
+    /// - Parameters:
+    ///   - screen0: The screen to place in the stack as root.
+    ///   - screen1: The screen to place in the stack as top.
+    /// - Returns: New `Screen` with replaced `viewControllers` in container.
     public func withStack<T0: Screen, T1: Screen>(
         _ screen0: T0,
         _ screen1: T1
@@ -60,7 +87,13 @@ extension Screen where Container: UINavigationController {
         )
     }
 
-    // swiftlint:disable:next generic_type_name
+    /// Replaces the view controllers currently managed by the navigation controller with the specified screen.
+    /// Sets the `viewControllers` property for a container with type `UINavigationController`.
+    /// - Parameters:
+    ///   - screen0: The screen to place in the stack as root.
+    ///   - screen1: The screen to place in the stack as second after the root.
+    ///   - screen2: The screen to place in the stack as top.
+    /// - Returns: New `Screen` with replaced `viewControllers` in container.
     public func withStack<T0: Screen, T1: Screen, T2: Screen>(
         _ screen0: T0,
         _ screen1: T1,
@@ -76,7 +109,14 @@ extension Screen where Container: UINavigationController {
         )
     }
 
-    // swiftlint:disable:next generic_type_name
+    /// Replaces the view controllers currently managed by the navigation controller with the specified screen.
+    /// Sets the `viewControllers` property for a container with type `UINavigationController`.
+    /// - Parameters:
+    ///   - screen0: The screen to place in the stack as root.
+    ///   - screen1: The screen to place in the stack as second after the root.
+    ///   - screen2: The screen to place in the stack as third after the root.
+    ///   - screen3: The screen to place in the stack as top.
+    /// - Returns: New `Screen` with replaced `viewControllers` in container.
     public func withStack<T0: Screen, T1: Screen, T2: Screen, T3: Screen>(
         _ screen0: T0,
         _ screen1: T1,
@@ -95,7 +135,15 @@ extension Screen where Container: UINavigationController {
         )
     }
 
-    // swiftlint:disable:next generic_type_name
+    /// Replaces the view controllers currently managed by the navigation controller with the specified screen.
+    /// Sets the `viewControllers` property for a container with type `UINavigationController`.
+    /// - Parameters:
+    ///   - screen0: The screen to place in the stack as root.
+    ///   - screen1: The screen to place in the stack as second after the root.
+    ///   - screen2: The screen to place in the stack as third after the root.
+    ///   - screen3: The screen to place in the stack as fourth after the root.
+    ///   - screen4: The screen to place in the stack as top.
+    /// - Returns: New `Screen` with replaced `viewControllers` in container.
     public func withStack<T0: Screen, T1: Screen, T2: Screen, T3: Screen, T4: Screen>(
         _ screen0: T0,
         _ screen1: T1,
@@ -116,5 +164,7 @@ extension Screen where Container: UINavigationController {
             screen4.eraseToAnyModalScreen()
         )
     }
+
+    // swiftlint:enable generic_type_name
 }
 #endif
