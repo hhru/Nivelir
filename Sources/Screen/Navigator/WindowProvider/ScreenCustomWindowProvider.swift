caf2bcd1435f4016b3814cac891ef2c6a1a81c61
Timur Shafigullin <hometim55@gmail.com>
2022-08-02 17:33:41 +0300

    Added documentation for WindowProvider

--- Sources/Screen/Navigator/WindowProvider/ScreenCustomWindowProvider.swift
+++ Sources/Screen/Navigator/WindowProvider/ScreenCustomWindowProvider.swift
@@ -1,8 +1,10 @@
 #if canImport(UIKit)
 import UIKit
 
+/// An implementation of `ScreenWindowProvider` that provides a `UIWindow` explicitly passed through the initializer.
 public struct ScreenCustomWindowProvider: ScreenWindowProvider {
 
+    /// The `UIWindow` for navigating and searching for containers.
     public private(set) weak var window: UIWindow?
 
     public init(window: UIWindow) {
