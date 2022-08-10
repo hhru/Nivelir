6448768ebc6f3701c01f2e4a3b37c3e7315ea37d
Timur Shafigullin <hometim55@gmail.com>
2022-08-10 12:32:54 +0300

    Added documentation about notification deep links

--- Sources/Deeplink/Notification/NotificationDeeplink.swift
+++ Sources/Deeplink/Notification/NotificationDeeplink.swift
@@ -2,15 +2,56 @@
 import Foundation
 import UserNotifications
 
+/// The type of ``Deeplink`` that is handled from the Notification.
+///
+/// This type allows you to handle the deep link from the notification data, which comes via function parameters.
+///
+/// Nivelir can decode `userInfo` depending on the type of ``NotificationUserInfo``.
+/// For example, if ``NotificationUserInfo`` implements the `Decodable` protocol,
+/// then there is no need to manually parse
+/// the dictionary of `userInfo` – check for keys and cast them to the correct type.
+/// Instead, the dictionary will be automatically decoded
+/// into an instance of the ``NotificationUserInfo`` type (like with JSON).
+/// The options for decoding are set via ``NotificationDeeplinkUserInfoOptions``.
+///
+/// You may need additional context to check and create an instance.
+/// For example, it may be a service factory from which additional data can be obtained.
+/// The associated type ``NotificationContext`` is used for this purpose.
 public protocol NotificationDeeplink: Deeplink, AnyNotificationDeeplink {
 
+    /// The type of user information associated with the notification.
     associatedtype NotificationUserInfo
+
+    /// Type of context for checking and creating a deep link instance.
     associatedtype NotificationContext
 
+    /// Options for decoding `userInfo` from notification.
+    ///
+    /// Implement this method if you want to change the data decoding strategies from `userInfo` notification.
+    /// To review the default options, see ``NotificationDeeplinkUserInfoOptions``.
+    ///
+    /// - Parameter context: Additional context for creating ``NotificationDeeplinkUserInfoOptions``.
+    /// - Returns: Returns new options for decoding `userInfo` from notification.
     static func notificationUserInfoOptions(
         context: NotificationContext
     ) -> NotificationDeeplinkUserInfoOptions
 
+    /// Creating a deep link from data of `UNNotificationResponse`.
+    ///
+    /// - Parameters:
+    ///   - requestIdentifier: [The unique identifier for this notification request.
+    ///   [Details](https://developer.apple.com/documentation/usernotifications/unnotificationrequest/1649634-identifier).
+    ///
+    ///   - categoryIdentifier: The identifier of the app-defined category object.
+    ///   [Details](https://developer.apple.com/documentation/usernotifications/unnotificationcontent/1649866-categoryidentifier).
+    ///
+    ///   - actionIdentifier: The identifier string of the action that the user selected.
+    ///   [Details](https://developer.apple.com/documentation/usernotifications/unnotificationresponse/1649548-actionidentifier).
+    ///
+    ///   - userInfo: Custom information associated with the notification.
+    ///   - context: Additional context for checking and creating instance.
+    /// - Returns: Returns a new instance of ``NotificationDeeplink`` that performs navigation.
+    /// Otherwise `nil` if the deep link cannot be handled.
     static func notification(
         requestIdentifier: String,
         categoryIdentifier: String,
@@ -19,6 +60,14 @@ public protocol NotificationDeeplink: Deeplink, AnyNotificationDeeplink {
         context: NotificationContext
     ) throws -> Self?
 
+    /// Creating a deep link from `UNNotificationResponse`.
+    ///
+    /// - Parameters:
+    ///   - response: The user’s response to an actionable notification.
+    ///   - userInfo: Custom information associated with the notification.
+    ///   - context: Additional context for checking and creating instance.
+    /// - Returns: Returns a new instance of ``NotificationDeeplink`` that performs navigation from `response` data.
+    /// Otherwise `nil` if the deep link from `response` cannot be handled.
     static func notification(
         response: UNNotificationResponse,
         userInfo: NotificationUserInfo,
@@ -66,12 +115,28 @@ extension NotificationDeeplink {
 
 extension NotificationDeeplink {
 
+    /// Options for decoding `userInfo` from notification.
+    ///
+    /// Implement this method if you want to change the data decoding strategies from `userInfo` notification.
+    /// To review the default options, see ``NotificationDeeplinkUserInfoOptions``.
+    ///
+    /// - Parameter context: Additional context for creating ``NotificationDeeplinkUserInfoOptions``.
+    /// - Returns: Returns new options for decoding `userInfo` from notification.
     public static func notificationUserInfoOptions(
         context: NotificationContext
     ) -> NotificationDeeplinkUserInfoOptions {
         NotificationDeeplinkUserInfoOptions()
     }
 
+    /// Options for decoding `userInfo` from notification.
+    ///
+    /// Implement this method if you want to change the data decoding strategies from `userInfo` notification.
+    /// To review the default options, see ``NotificationDeeplinkUserInfoOptions``.
+    ///
+    /// - Parameter context: Additional context for creating ``NotificationDeeplinkUserInfoOptions``.
+    /// Must match the context type of ``NotificationDeeplink/NotificationContext``.
+    ///
+    /// - Returns: Returns new options for decoding `userInfo` from notification.
     public static func notificationUserInfoOptions(
         context: Any?
     ) throws -> NotificationDeeplinkUserInfoOptions {
--- Sources/Deeplink/Notification/NotificationDeeplinkUserInfoDecoder.swift
+++ Sources/Deeplink/Notification/NotificationDeeplinkUserInfoDecoder.swift
@@ -1,6 +1,32 @@
 #if os(iOS)
 import Foundation
 
+/// A type that decodes instances of a data type from the dictionary.
+///
+/// The example below shows how to decode an instance of a simple GroceryProduct type from the dictionary.
+/// The type adopts Codable so that it’s decodable using a instance of ``NotificationDeeplinkUserInfoDecoder``.
+///
+/// ```swift
+/// struct GroceryProduct: Codable {
+///     var name: String
+///     var points: Int
+///     var description: String?
+/// }
+///
+/// let dictionary = [
+///     "name": "Durian",
+///     "points": 600,
+///     "description": "A fruit with a distinctive scent."
+/// ]
+///
+/// let decoder: NotificationDeeplinkUserInfoDecoder
+/// let product = try decoder.decode(GroceryProduct.self, from: dictionary)
+///
+/// print(product.name) // Prints "Durian"
+/// ```
+///
+/// - SeeAlso: ``NotificationDeeplink``
+/// - SeeAlso: ``NotificationDeeplinkUserInfoOptions``
 public protocol NotificationDeeplinkUserInfoDecoder {
 
     func decode<T: Decodable>(_ type: T.Type, from dictionary: [String: Any]) throws -> T
