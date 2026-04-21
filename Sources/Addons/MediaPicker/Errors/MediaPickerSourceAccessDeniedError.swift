1004b703b69f552c1ed3cf23d190fa36f0edadcc
Almaz Ibragimov <almazrafi@gmail.com>
2026-04-21 10:49:16 +0300

    MediaPicker improvements (#162)

--- Sources/Addons/MediaPicker/Errors/MediaPickerSourceAccessDeniedError.swift
+++ Sources/Addons/MediaPicker/Errors/MediaPickerSourceAccessDeniedError.swift
@@ -4,19 +4,33 @@ import Foundation
 public struct MediaPickerSourceAccessDeniedError: ScreenError {
 
     public let description: String
+    public let isRequested: Bool
 
-    public init(for trigger: Any) {
+    public init(
+        for trigger: Any,
+        isRequested: Bool = false
+    ) {
         description = """
         User does not allow the app to access the media source for:
           \(trigger)
         """
+
+        self.isRequested = isRequested
     }
 }
 
 extension Result where Failure == Error {
 
-    internal static func mediaPickerSourceAccessDenied(for trigger: Any) -> Self {
-        .failure(MediaPickerSourceAccessDeniedError(for: trigger))
+    internal static func mediaPickerSourceAccessDenied(
+        for trigger: Any,
+        isRequested: Bool = false
+    ) -> Self {
+        .failure(
+            MediaPickerSourceAccessDeniedError(
+                for: trigger,
+                isRequested: isRequested
+            )
+        )
     }
 }
 #endif
